2ad55dcd5079ea3bdceab02361d216a89660e759
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-09-11 12:24:06 -0400

    added search functionality
    still needs work

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
 import { DOMSelectors } from "./DOM";
 
-/*const card = document.getElementById("card");
-
-card.addEventListener("click", flipCard);
-
-function flipCard() {
-  card.classList.toggle("flipCard");
-}
-*/
-
 const myHeaders = new Headers({
   "x-api-key": "23a9022c-c652-4ef8-8427-9c8d230601aa",
 });
---
+++ src/js/search.js
@@ -0,0 +1,40 @@
+import { DOMSelectors } from "./DOM";
+
+const myHeaders = new Headers({
+  "x-api-key": "23a9022c-c652-4ef8-8427-9c8d230601aa",
+});
+
+const listen = function () {
+  DOMSelectors.searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    DOMSelectors.grid.innerHTML = "";
+    const searchParams = DOMSelectors.searchArea.value;
+    const searchQuery = async function () {
+      try {
+        const response = await fetch(
+          `https://api.thecatapi.com/v1/images/search?breed_ids=${searchParams}`
+        );
+        const data = await response.json();
+        console.log(data);
+        data.forEach((cats) => {
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend",
+            `<div class="item-card" id="card">
+              <div class="item-card-front">
+                  <img src="${cats.url}" alt="" class="image">
+              </div>
+              <div class="item-card-back">
+              </div>
+      </div>`
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        alert("Hey something went wrong");
+      }
+    };
+    searchQuery();
+  });
+};
+
+listen();
